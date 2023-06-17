d11406a7a4ea8d1aa0d8c5b87308a6ee814b5199
pateldivyam26 <79200448+pateldivyam26@users.noreply.github.com>
2023-06-17 14:11:37 +0530

    update skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Section from "../components/Section";
 import {
   SiC,
+  SiCplusplus,
   SiAndroidstudio,
   SiCss3,
   SiDocker,
@@ -10,7 +11,7 @@ import {
   SiFlutter,
   SiGit,
   SiGithub,
-  SiGitlab,
+  SiNodedotjs,
   SiHtml5,
   SiJavascript,
   SiKeras,
@@ -21,31 +22,30 @@ import {
   SiReact,
   SiTensorflow,
 } from "react-icons/si";
-import { BsTerminalFill } from "react-icons/bs";
 import "../css/Skill.css";
 
 const Skills = () => {
   const icons = [
     { icon: <SiC />, name: "C", color: "#A8B9CC" },
+    { icon: <SiCplusplus />, name: "C++", color: "#00599C" },
     { icon: <SiPython />, name: "Python", color: "#3776AB" },
+    { icon: <SiHtml5 />, name: "HTML5", color: "#E34F26" },
+    { icon: <SiCss3 />, name: "CSS3", color: "#1572B6" },
+    { icon: <SiJavascript />, name: "JavaScript", color: "#F7DF1E" },
     { icon: <SiGit />, name: "Git", color: "#F05032" },
     { icon: <SiGithub />, name: "GitHub", color: "#181717" },
-    { icon: <SiGitlab />, name: "GitLab", color: "#FCA121" },
-    { icon: <SiJavascript />, name: "JavaScript", color: "#F7DF1E" },
     { icon: <SiTensorflow />, name: "TensorFlow", color: "#FF6F00" },
     { icon: <SiKeras />, name: "Keras", color: "#D00000" },
     { icon: <SiFlask />, name: "Flask", color: "#000000" },
     { icon: <SiFlutter />, name: "Flutter", color: "#02569B" },
-    { icon: <SiHtml5 />, name: "HTML5", color: "#E34F26" },
-    { icon: <SiCss3 />, name: "CSS3", color: "#1572B6" },
     { icon: <SiReact />, name: "React", color: "#61DAFB" },
+    { icon: <SiNodedotjs />, name: "NodeJS", color: "#339933" },
     { icon: <SiMongodb />, name: "MongoDB", color: "#47A248" },
     { icon: <SiPostgresql />, name: "PostgreSQL", color: "#336791" },
     { icon: <SiFirebase />, name: "Firebase", color: "#FFCA28" },
     { icon: <SiDocker />, name: "Docker", color: "#2496ED" },
     { icon: <SiAndroidstudio />, name: "Android Studio", color: "#3DDC84" },
     { icon: <SiPostman />, name: "Postman", color: "#FF6C37" },
-    { icon: <BsTerminalFill />, name: "Terminal", color: "#4D4D4D" },
   ];
 
   return (
